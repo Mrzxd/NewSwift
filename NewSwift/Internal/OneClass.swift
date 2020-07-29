6125c28a18e4d8b1506e97da1e6ca396cd6228b3
CXX <457742782@qq.com>
2020-07-29 20:10:29 +0800

    e

---
+++ NewSwift/Internal/OneClass.swift
@@ -0,0 +1,9 @@
+//
+//  OneClass.swift
+//  NewSwift
+//
+//  Created by CXX on 2020/7/29.
+//  Copyright © 2020 张兴栋. All rights reserved.
+//
+
+import Foundation
---
+++ NewSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewSwift
+//
+//  Created by CXX on 2020/7/29.
+//  Copyright © 2020 张兴栋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
